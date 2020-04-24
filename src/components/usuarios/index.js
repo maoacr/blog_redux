76d2b141848e8811ecc02ac68c5ba39a121aea94
maoacr <maoacr@gmail.com>
2020-04-24 17:17:59 -0500

    Fases extra, try catch, manejando estados de la app

--- src/components/usuarios/index.js
+++ src/components/usuarios/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Spinner from '../General/Spinner';
+import Fatal from '../General/Fatal';
+import Tabla from './Tabla';
 
 import * as usuariosActions from '../../actions/usuariosActions';
 
@@ -9,41 +12,23 @@ class Usuarios extends Component {
 		this.props.traerTodos();
 	}
 
-	ponerFilas = () => this.props.usuarios.map((usuario) => (
-		<tr key={ usuario.id }>
-			<td>
-				{ usuario.name }
-			</td>
-			<td>
-				{ usuario.email }
-			</td>
-			<td>
-				{ usuario.website }
-			</td>
-		</tr>
-	));
+	ponerContenido = () => {
+		if (this.props.cargando) {
+			return <Spinner />;
+		}
+
+		if (this.props.error) {
+			return <Fatal mensaje={ this.props.error } />;
+		}
+
+		return <Tabla usuarios={this.props.usuarios} />
+	};
 
 	render() {
 		return (
 			<div>
-				<table className="tabla">
-					<thead>
-						<tr>
-							<th>
-								Nombre
-							</th>
-							<th>
-								Correo
-							</th>
-							<th>
-								Enlace
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						{ this.ponerFilas() }
-					</tbody>
-				</table>
+				<h1>Usuarios</h1>
+				{ this.ponerContenido() }
 			</div>
 		)
 	}
