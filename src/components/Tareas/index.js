f42740a88b05f585bfd693c88140a297a71d86da
maoacr <maoacr@gmail.com>
2020-04-25 20:25:33 -0500

    Ciclo redux para Tareas completo

---
+++ src/components/Tareas/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as tareasActions from '../../actions/tareasActions'; 
+
+class Tareas extends Component {
+
+	componentDidMount(){
+		this.props.traerTodas();
+	}
+
+	render() {
+		console.log(this.props)
+		return (
+			<div>
+				Tareas Component
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = ({ tareasReducer }) => tareasReducer;
+
+export default connect(mapStateToProps, tareasActions)(Tareas);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import usuariosReducer from './usuariosReducer';
+import tareasReducer from './tareasReducer';
 import publicacionesReducer from './publicacionesReducer';
 
 export default combineReducers({
 	usuariosReducer,
-	publicacionesReducer
+	publicacionesReducer,
+	tareasReducer
 });
